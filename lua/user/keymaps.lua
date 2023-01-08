4ddc8a95f817016b1b40a5c2df40771978a266bb
meomeocoj <tminh1103@gmail.com>
2023-01-08 16:11:05 +0700

    config the null ls

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,7 +1,6 @@
 local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
-
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
@@ -24,6 +23,10 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Save the file 
+
+keymap("n", "<C-s>", ":w<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
